706e6f6f93b2d6247b3fd088eaa42790fcd948bb
Sumit Kashyap <mr.sumitkrr@gmail.com>
2023-04-19 18:47:55 +0530

    Update index.py

--- index.py
+++ index.py
@@ -24,14 +24,14 @@ images = os.path.join("images")
 os.listdir(images)
 
 train_generator = train_datagen.flow_from_directory(
-    images,
+    "images",
     target_size=(224, 224),
     batch_size=32,
     class_mode='categorical'
 )
 
 val_generator = val_datagen.flow_from_directory(
-    images,
+    "images",
     target_size=(224, 224),
     batch_size=32,
     class_mode='categorical'
